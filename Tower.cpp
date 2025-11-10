a9caa3c719da1fb5330ade1c3fabdabc113e0efe
yunchen233 <745320361@qq.com>
2025-11-11 01:30:22 +0800

    针对Enemy功能留下接口

--- Tower.cpp
+++ Tower.cpp
@@ -2,18 +2,19 @@
 #include "Bullet.h"
 #include "Enemy.h"
 #include <QLineF>
-#include <QGraphicsScene>
 
 Tower::Tower(int damage, double range, double fireRate,int cost,int upgradeCost,const QPixmap& pixmap, QGraphicsItem* parent)
     : QObject(nullptr),
-      QGraphicsPixmapItem(pixmap, parent),
-      damage(damage),
-      range(range),
-      fireRate(fireRate),
-      cost(cost),
-      upgradeCost(upgradeCost),
-      upgraded(false),
-      currentTarget(nullptr),fireTimer(nullptr) {
+    QGraphicsPixmapItem(pixmap, parent),
+    damage(damage),
+    range(range),
+    fireRate(fireRate),
+    cost(cost),
+    upgradeCost(upgradeCost),
+    upgraded(false),
+    currentTarget(nullptr),fireTimer(nullptr)
+{
+    setPixmap(pixmap);
     if(fireRate>0)//为了后续不需要fireTimer的情况方便传入fireRate数据
     {
         fireTimer = new QTimer(this);
@@ -26,14 +27,6 @@ void Tower::setTarget(Enemy* target) {
     currentTarget = target;
 }
 
-void Tower::findAndAttackTarget() {
-    if (currentTarget && targetIsInRange()) {
-        attack();
-    }
-    //如果没有目标或者目标超出范围，GameManager会负责为其寻找新目标
-}
-
-
 bool Tower::targetIsInRange() const {
     if (!currentTarget) {
         return false;
@@ -42,14 +35,34 @@ bool Tower::targetIsInRange() const {
     return line.length() <= range;
 }
 
+void Tower::findAndAttackTarget() {
+    if (currentTarget && targetIsInRange()) {
+        attack();
+    }
+    //如果没有目标或者目标超出范围，GameManager会负责为其寻找新目标
+}
+
 void Tower::attack() {
     if (currentTarget) {
         emit newBullet(this, currentTarget);
     }
 }
 
-void Tower::upgrade() {
-    if (upgraded) {
-        return; // 已经升级过，不能再次升级
+void Tower::destroy(){
+    if(fireTimer!=nullptr)
+        fireTimer->stop();
+     emit towerDestroyed(this);
+}
+
+void Tower::slowAttack(double slowFactor){
+    //GameManager会负责检测周围是否有回忆怪物
+    if(fireTimer!=nullptr){
+        fireTimer->stop();
+        if(fireRate>0)
+        {
+            fireTimer->start(1000/(fireRate*slowFactor));
+        }
     }
 }
+
+
