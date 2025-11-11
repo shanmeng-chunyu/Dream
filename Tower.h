85655bc99bae802b26cc11dfbe12268312cfc72f
yunchen233 <745320361@qq.com>
2025-11-12 00:02:20 +0800

    删除fireTimer，改用int类；增加光环类Tower相关

--- Tower.h
+++ Tower.h
@@ -4,10 +4,12 @@
 #include <QObject>
 #include <QGraphicsPixmapItem>
 #include <QTimer>
+#include <Qstring>
 #include "Enemy.h" // 包含头文件以使用Enemy类
 
 class Bullet;
-
+class LiveCoffee;
+class FriendCompanion;
 class Tower : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
@@ -22,13 +24,12 @@ public:
     int getUpgradeCost()const{return upgradeCost;}
     Enemy* getCurrentTarget()const{return currentTarget;}
     bool IsUpgraded()const{return upgraded;}
-    QString getType()const{return type;}
-    void setRange(int newrange){range=newrange;}
 
     //基本功能
     virtual void attack();
     bool targetIsInRange() const;//检查敌人是否在攻击范围内
     void setTarget(Enemy* target);
+    void setRange(int newrange){range=newrange;}
 
     //升级
     virtual void upgrade();
@@ -37,11 +38,19 @@ public slots:
     void findAndAttackTarget();
     //应对enemy功能
     void slowAttack(double slowFactor);//降低攻速
+    void slowAttackStop();//停止降低攻速
     void destroy();//摧毁tower，接收到信号直接删除对象
 
 signals:
     void newBullet(Tower* tower, Enemy* target);
     void towerDestroyed(Tower* tower);  //由GameManager处理实际删除
+
+private:
+    //光环类tower检测，光环类Tower不需要这些函数
+    void coffeeEffect();//实现
+    void friendEffect();
+    QList<LiveCoffee*> findCoffeeInRange();//查找
+    QList<FriendCompanion*> findFriendInRange();
 
 protected:
     //属性
@@ -51,13 +60,16 @@ protected:
     int cost;
     int upgradeCost;
     bool upgraded;//升级标志
-    QString type;
 
     //状态属性
     Enemy* currentTarget;
-    QTimer* fireTimer;
+    int fireInterval;//开火间隔(帧数)
+    int fireCount;//距离下次攻击的帧数
+
+    //为了防止无限叠加，用于记录原始数值的变量
+    int originalFireInterval;
+    int originalDamage;
 
     friend class GameManager;
 };
 #endif // TOWER_H
-
