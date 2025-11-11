1c8d216670e214a5ee6ff82fdeae1b2881065906
yunchen233 <745320361@qq.com>
2025-11-11 16:01:28 +0800

    修正路径，修正fireTimer逻辑

--- InspirationBulb.cpp
+++ InspirationBulb.cpp
@@ -1,24 +1,21 @@
 #include "InspirationBulb.h"
 #include<QPixmap>
 
-InspirationBulb::InspirationBulb(int range,QGraphicsItem* parent):Tower(40,range,0.8,100,150,QPixmap("resources/towers/level1/InspirationBulb.png"),parent)
-{
-    upgradedPixmapPath = "resources/towers/level1/InspirationBulb_upgrade.png";
-}
+InspirationBulb::InspirationBulb(int range,QGraphicsItem* parent):Tower(40,range,0.8,100,150,QPixmap(":/towers/resources/towers/level1/InspirationBulb.png"),parent){}
 void InspirationBulb::upgrade()
 {
-//好像没有升级后的数据先随便设置了一点
     if(!upgraded)
     {
         damage=60;
         range=4.0;
         fireRate=0.6;
         upgraded=true;
-        setPixmap(QPixmap(upgradedPixmapPath));
+        setPixmap(QPixmap( ":/towers/resources/towers/level1/InspirationBulb_upgrade.png"));
         if(fireTimer)
         {
             fireTimer->stop();
-            fireTimer->start(1000/fireRate);
+            fireTimer->start(1000*fireRate);
         }
     }
 }
+
