bfac46c8f41fc2973f93c935921cabbb3378c43b
shanmengchunyu <2310495783@qq.com>
2025-11-11 10:37:04 +0800

    更新地图素材

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <QApplication>
+#include <ui_widget_menu.h>
+
 #include "MainWindow.h"
 #include "LevelEditorWidget.h"
+#include "widget_choose_level.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     // MainWindow w;
     // w.show();
+
+    //用于调试主菜单的代码
+    // Ui::widget_menu menu_ui;
+    // QWidget menu_widget;
+    // menu_ui.setupUi(&menu_widget);
+    // menu_widget.setWindowTitle("Level3 Main Menu (Menu Debug)");
+    // menu_widget.resize(1024, 768); // 调整为你认为合适的大小
+    // menu_widget.show();
+
+    //用于调试关卡选择界面的代码
+    // widget_choose_level choose_level_widget;
+    // choose_level_widget.setWindowTitle("Level3 Choose Level (Choose Level Debug)");
+    // choose_level_widget.resize(1024, 768); // 调整为你认为合适的大小
+    // choose_level_widget.show();
 
     // --- 用于调试编辑器的新代码 ---
     // LevelEditorWidget editor;
