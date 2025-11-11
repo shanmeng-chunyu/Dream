bb13be6c4f4978eb409f079063a707370087f467
yunchen233 <745320361@qq.com>
2025-11-12 00:06:33 +0800

    删除Timer，根据基类调整

--- FishingCatPillow.cpp
+++ FishingCatPillow.cpp
@@ -3,38 +3,29 @@
 #include <QLineF>
 #include <QGraphicsScene>
 
-FishingCatPillow::FishingCatPillow(double range,QGraphicsItem* parent):Tower(0,range,0,120,180,QPixmap( ":/towers/resources/towers/level1/FishingCatPillow.png"),parent)
+FishingCatPillow::FishingCatPillow(QGraphicsItem* parent):Tower(0,2.5,5,120,180,QPixmap( ":/towers/resources/towers/level1/FishingCatPillow.png"),parent)
 {
     controlDuration=1.5;
-    controlInterval=5.0;
-    //设置控制效果定时器
-    controlTimer=new QTimer(this);
-    connect(controlTimer,&QTimer::timeout,this,&FishingCatPillow::controlTarget);
-    controlTimer->start(controlInterval*1000);
 }
-void FishingCatPillow::controlTarget()
-{
-    //如果存在目标，发射信号冷冻enemy
+void FishingCatPillow::attack()
+{    //如果存在目标，发射信号冷冻enemy
     if(currentTarget)
     {
         emit applyControl(currentTarget,controlDuration);
     }
-
 }
 void FishingCatPillow::upgrade()
 {
     if(!upgraded)
     {
         controlDuration=2.0;
-        controlInterval=4.0;
+        fireRate=4.0;
+        fireInterval=fireRate*60;
+        originalFireInterval=fireInterval;
+        originalDamage=damage;
+        if(fireCount>fireInterval)
+            fireCount=fireInterval;
         upgraded=true;
         setPixmap(QPixmap(":/towers/resources/towers/level1/FishingCatPillow_upgrade.png"));
-        if(controlTimer)
-        {
-            controlTimer->stop();
-            controlTimer->start(controlInterval*1000);
-        }
     }
 }
-
-
