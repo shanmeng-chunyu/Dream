a7c68eded7a694f8dd56767a4cfc086d63a13442
yunchen233 <745320361@qq.com>
2025-11-12 00:07:35 +0800

    增加光环类Tower

---
+++ FriendCompanion.h
@@ -0,0 +1,16 @@
+#ifndef FRIENDCOMPANION_H
+#define FRIENDCOMPANION_H
+
+#include "Tower.h"
+
+class FriendCompanion:public Tower
+{
+public:
+    explicit FriendCompanion(QGraphicsItem* parent = nullptr);
+    void upgrade() override;
+    double getFactor(){return increaseDamage;};
+private:
+    double increaseDamage;
+};
+
+#endif // FRIENDCOMPANION_H
