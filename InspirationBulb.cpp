368c0b9aecfd7bf59d7a8b64fafc47830f6fe746
yunchen233 <745320361@qq.com>
2025-11-12 00:05:23 +0800

    根据基类做出调整

--- InspirationBulb.cpp
+++ InspirationBulb.cpp
@@ -1,21 +1,20 @@
 #include "InspirationBulb.h"
 #include<QPixmap>
 
-InspirationBulb::InspirationBulb(double range,QGraphicsItem* parent):Tower(40,range,0.8,100,150,QPixmap(":/towers/resources/towers/level1/InspirationBulb.png"),parent){}
+InspirationBulb::InspirationBulb(QGraphicsItem* parent):Tower(40,2.5,0.8,100,150,QPixmap(":/towers/resources/towers/level1/InspirationBulb.png"),parent){}
 void InspirationBulb::upgrade()
 {
     if(!upgraded)
     {
         damage=60;
-        range=4.0;
+        range=3.5;
         fireRate=0.6;
         upgraded=true;
-        setPixmap(QPixmap( ":/towers/resources/towers/level1/InspirationBulb_upgrade.png"));
-        if(fireTimer)
-        {
-            fireTimer->stop();
-            fireTimer->start(1000*fireRate);
-        }
+        fireInterval=fireRate*60;
+        originalFireInterval=fireInterval;
+        originalDamage=damage;
+        if(fireCount>fireInterval)
+            fireCount=fireInterval;
+        setPixmap(QPixmap(":/towers/resources/towers/level1/InspirationBulb_upgrade.png"));
     }
 }
-
