30284f44801091099d56afd3aefa9b0c512c644b
yunchen233 <745320361@qq.com>
2025-11-13 09:20:16 +0800

    修复贴图

--- FishingCatPillow.cpp
+++ FishingCatPillow.cpp
@@ -31,7 +31,11 @@ void FishingCatPillow::upgrade()
         if(fireCount>fireInterval)
             fireCount=fireInterval;
         upgraded=true;
-        setPixmap(QPixmap(":/towers/resources/towers/level1/FishingCatPillow_upgrade.png"));
+        const QSize towerPixelSize(76, 76);
+        QPixmap originalUpgradePixmap(":/towers/resources/towers/level1/FishingCatPillow_upgrade.png");
+        QPixmap scaledPixmap = originalUpgradePixmap.scaled(towerPixelSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        setPixmap(scaledPixmap);
     }
 }
+
 
